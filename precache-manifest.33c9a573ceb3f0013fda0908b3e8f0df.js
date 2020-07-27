1df7ec86eb9eb27daadb765b57dad143827b1fb7
mohammedfarisofficial <mohammedfarisofficial@gmail.com>
2020-07-27 12:02:48 +0530

    Updates

---
+++ precache-manifest.33c9a573ceb3f0013fda0908b3e8f0df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eea4cae80eac0fe525d56b6afa1ca77",
+    "url": "/react-tudu/index.html"
+  },
+  {
+    "revision": "7a7d06901fc6510acdf0",
+    "url": "/react-tudu/static/css/main.2f3de2b6.chunk.css"
+  },
+  {
+    "revision": "97d0825eb2f316bfad69",
+    "url": "/react-tudu/static/js/2.754f03fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tudu/static/js/2.754f03fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a7d06901fc6510acdf0",
+    "url": "/react-tudu/static/js/main.02ed7a2c.chunk.js"
+  },
+  {
+    "revision": "06b85c0645b65d485d7b",
+    "url": "/react-tudu/static/js/runtime-main.9c94774d.js"
+  }
+]);
